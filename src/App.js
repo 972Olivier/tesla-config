5b2e8e55ad9ccaafdabeaf57a913e9fef9ddf954
972Olivier <oliviercen@gmail.com>
2022-07-05 10:27:59 -0400

    exo is ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+
+import Tesla from "./asset/Tesla.json";
+import { useState } from "react";
+const data = Tesla;
 
 function App() {
+  const container1 = 90700;
+  const container2 = 106700;
+
+  const [result, setResult] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title={"Tesla config"}></Header>
+      <main>
+        <section>
+          <h2>{data[0].title}</h2>
+          <div
+            onClick={() => {
+              setResult(container1);
+            }}
+            className={`container-1`}
+          >
+            <p>
+              <span>{data[0].box[0].description}</span> -
+              <span> {data[0].box[0].price}</span> €
+            </p>
+          </div>
+          <div
+            onClick={() => {
+              setResult(container2);
+            }}
+            className={`container-2`}
+          >
+            <p>
+              <span>{data[0].box[1].description}</span> -
+              <span> {data[0].box[1].price}</span> €
+            </p>
+          </div>
+          <h2>{data[1].title}</h2>
+          <div
+            onClick={() => {
+              result === 90700 || result === 106700
+                ? setResult(result + 0)
+                : setResult(result - 1000);
+            }}
+            className={`container-3`}
+          >
+            <p>
+              <span>{data[1].box[0].description}</span> -
+              <span> {data[1].box[0].price}</span> €
+            </p>
+          </div>
+          <div
+            onClick={() => {
+              result === 90700 || result === 106700
+                ? setResult(result + 1000)
+                : setResult(result + 0);
+            }}
+            className={`container-4`}
+          >
+            <p>
+              <span>{data[1].box[1].description}</span> -
+              <span> {data[1].box[1].price}</span> €
+            </p>
+          </div>
+        </section>
+        <div>
+          <span className="result">{result} €</span>
+          <button
+            onClick={() => {
+              return alert("Thanks for clicking !");
+            }}
+          >
+            Buy !
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
